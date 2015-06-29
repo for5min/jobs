008f11f5dc88f61c57afc3fb2e871d6e2059a914
Alex Tu <alex.lei.tu@gmail.com>
2015-06-29 14:16:42 +0800

    change to new

--- 09_infoblox/loopadd.sh
+++ 09_infoblox/loopadd.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-if [ $# != 1 ];then
-  echo "Please specific the aboslute path of the file"
-  exit 1
+if [ $# != 2 ];then
+    echo "Usage: loopadd.sh record.txt change-id"
+    exit 1
 fi
 
 if [ ! -f $1 ];then
-  echo "Your file is not exsiting"
-  exit 1
+    echo "Your file is not exsiting"
+    exit 1
 fi
 
 LINE=`awk 'END{print NR}' $1`
 
 for ((i=1; i<=$LINE; i++))
 do
-  export HOST=`awk -v a=$i 'NR==a {print $1}' $1`
-  export CNAME=`awk -v a=$i 'NR==a {print $2}' $1`
-  #  echo "aaa $HOST -t $CNAME"
-  /usr/local/bin/infoblox-record-add -t cname -n $HOST -v $CNAME
+   export CNAME=`awk -v a=$i 'NR==a {print $1}' $1`
+   export HOST=`awk -v a=$i 'NR==a {print $2}' $1`
+      #  echo "aaa $HOST -t $CNAME"
+  /usr/local/bin/infoblox-record-add-improve -t cname -n $CNAME -v $HOST -c $2
 done
